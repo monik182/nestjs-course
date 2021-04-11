5e894b5c58de2fc76ae4c8cd55e15294bbfb91c7
Monica Carvajal <mcarvajal@leal.co>
2021-04-10 20:19:22 -0500

    chapter 4 and 5

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,10 +5,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffee.constants';
+import { ConfigModule } from '@nestjs/config';
+import coffeesConfig from '../config/coffees.config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
+  imports: [
+    TypeOrmModule.forFeature([Coffee, Flavor, Event]),
+    ConfigModule.forFeature(coffeesConfig)
+  ],
   controllers: [CoffeesController],
-  providers: [CoffeesService]
+  providers: [
+    CoffeesService,
+    // {provide: COFFEE_BRANDS, useValue: ['Nestle', 'Oma']},
+    {
+      provide: COFFEE_BRANDS,
+      useFactory: () => ['Nestle', 'Oma'],
+    }
+    ],
+  exports: [CoffeesService]
 })
 export class CoffeesModule {}
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Connection, Repository } from 'typeorm';
@@ -7,6 +7,9 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Flavor } from './entities/flavor.entity';
 import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 import { Event } from '../events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffee.constants';
+import { ConfigService, ConfigType } from '@nestjs/config';
+import coffeesConfig from '../config/coffees.config';
 
 @Injectable()
 export class CoffeesService {
@@ -16,7 +19,19 @@ export class CoffeesService {
     @InjectRepository(Flavor)
     private readonly flavorRepository: Repository<Flavor>,
     private readonly connection: Connection,
-  ) {}
+    @Inject(COFFEE_BRANDS) coffeeBrands: string[],
+    private readonly configService: ConfigService,
+    @Inject(coffeesConfig.KEY)
+    private readonly coffeesConfiguration: ConfigType<typeof coffeesConfig>
+  ) {
+    const dbHost = this.configService.get<string>(
+      // 'DATABASE_HOST'
+      'database.host'
+    );
+    const coffeeConfig = this.configService.get<string>('coffees');
+    const foo = this.configService.get<string>('coffees.foo');
+    console.log({dbHost, coffeeConfig, foo, coffeesConfiguration});
+  }
 
   async findAll(paginationQuery: PaginationQueryDto) {
     const { limit: take, offset: skip } = paginationQuery;
